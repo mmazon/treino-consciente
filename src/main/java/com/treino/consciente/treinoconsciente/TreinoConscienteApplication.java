d40e0271dfa8ac89563dd7495f0a5427c2c3c3aa
Moacir Mazon <moacir.mazon@gmail.com>
2018-08-10 15:35:56 -0300

    inicio projeto

---
+++ src/main/java/com/treino/consciente/treinoconsciente/TreinoConscienteApplication.java
@@ -0,0 +1,12 @@
+package com.treino.consciente.treinoconsciente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinoConscienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinoConscienteApplication.class, args);
+	}
+}
